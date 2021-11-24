b188a44366642f27fcd9ec0ea762b2a344a81235
안영훈 <wsxc9468@gmail.com>
2021-11-24 17:56:32 +0900

    주석 추가

--- UtilFunction/UtilFunctionAPI.h
+++ UtilFunction/UtilFunctionAPI.h
@@ -6,11 +6,7 @@
 #define DLL_UTIL __declspec(dllimport)
 #endif // _DLL_UTIL_
 
-/*******************************
-********* 외부 추가 헤더 ********
-********************************/
-
-
+// 사용자 유틸 함수들
 DLL_UTIL char* ConvertWCtoC(wchar_t* str);								// wchar_t -> char 변수
 DLL_UTIL wchar_t* ConvertCharToWC(char* str);							// char -> wchar_t
 DLL_UTIL bool compareString(std::string a, std::string b);							// string 대문자 변환 및 비교
